509c0a3ad2bd6bd34d9b5ebd5e699278b40dc0ce
Truong Quang Huy <kzyo5962@gmail.com>
2022-02-11 01:45:36 +0700

    set up ui

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,7 +1,28 @@
+import LoginForm from 'components/Auth/LoginForm/LoginForm';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../styles/auth.scss';
 
 const Login = () => {
-  return <div>Login screen</div>;
+  return (
+    <div className="auth_page">
+      <div className="auth_box">
+        <h3 className="text-uppercase text-center mb-4">Login</h3>
+        <LoginForm />
+
+        <small className="row my-2 text-primary" style={{ cursor: 'pointer' }}>
+          <span className="col-6">
+            <Link to="/forgot_password">Forgot password?</Link>
+          </span>
+        </small>
+
+        <p>
+          You don't have an account?&nbsp;
+          <Link to={`/register`}>Register Now</Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
